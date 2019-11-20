bb2fe5527a029027d6b556b74ee1807d1dcc1f13
joelboeder <joel.e.boeder@wellsfargo.com>
2019-11-20 13:49:40 -0600

    Added jest

--- web/tests/unit/basic.spec.js
+++ web/tests/unit/basic.spec.js
@@ -10,6 +10,18 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
-};
+import { shallowMount } from '@vue/test-utils'
+import App from "@/App.vue";
+import router from "@/router.js";
+import store from "@/store.js";
+// Note: main.js does not work correctly with jest for unknown reasons and is not part of the test
+// at this point in time.
+
+// Jest requires one test case - for the time being it always returns true (a cat is a cat) but
+// it also causes syntax check for all the Javascript/Vue files.  Future expansion will add
+// test cases.
+describe('Main', () => {
+  it('compiles ', () => {
+    expect("cat").toMatch("cat")
+  })
+})

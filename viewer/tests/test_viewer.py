e3436c28923d15d3c878d99f7d25cb814d927965
Joelebwf <joel.e.boeder@wellsfargo.com>
2019-11-21 15:41:39 -0600

    Fixed test cases and added both python and node.js

--- viewer/tests/test_viewer.py
+++ viewer/tests/test_viewer.py
@@ -18,14 +18,14 @@ class TestViewer(unittest.TestCase):
 
     def test_entrypoints(self):
         data = json.loads(viewer.entrypoints())
-        self.assertEqual(160, len(data))
+        self.assertEqual(161, len(data))
         self.assertTrue("entrypoint" in data[0])
         self.assertTrue("type" in data[0])
         self.assertTrue("description" in data[0])
 
     def test_concepts(self):
         data = json.loads(viewer.concepts())
-        self.assertEqual(3333, len(data))
+        self.assertEqual(3404, len(data))
         self.assertTrue("name" in data[0])
         self.assertTrue("taxonomy" in data[0])
         self.assertTrue("datatype" in data[0])
